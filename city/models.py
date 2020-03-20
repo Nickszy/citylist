287ec51fc4424de87ccce0a8d5986f57c65df33f
nickszy <1344398321@qq.com>
2020-03-20 18:05:05 +0800

    add city_des model

--- city/models.py
+++ city/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    city = models.CharField(max_length=100, primary_key=True)
+    # ename = models.CharField()
+    des = models.TextField()
+    tags = models.TextField()
+
+    class Meta:
+        db_table = 'city_des'
+
+# class City_data(models.Model):
+#     class META:
+#         db_table = 'city_data'
+
+#     medical = models.FloatField()
+
+#     dict()
+
